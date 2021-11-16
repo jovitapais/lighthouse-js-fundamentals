564ef3d24126f6927af4f14ab07fc331d753087e
Jovita <jovitaspais@gmail.com>
2021-11-16 21:33:23 +0000

    another code for loopy lighthouse

---
+++ loopy-lighthouse2.js
@@ -0,0 +1,14 @@
+let lastIndexOf = function(item,index) {
+  //console.log(item)
+  let value = -1;
+    if (item.lastIndexOf(index)) {
+      value = (item.lastIndexOf(index))
+
+ }return value;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);

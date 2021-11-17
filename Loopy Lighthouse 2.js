11ae967cf041fb3dc99edfb15f605235d70b03f8
Jovita <74214617+jovitapais@users.noreply.github.com>
2021-11-17 12:47:14 -0500

    Update Loopy Lighthouse 2.js

--- Loopy Lighthouse 2.js
+++ Loopy Lighthouse 2.js
@@ -1,5 +1,5 @@
 let loopyLighthouse = function(range, multiples, words) {
-  for (let i=range[0];i<=range[1];i++) {
+  for (let i = range[0]; i <= range[1] ; i++ ) {
     if (i % multiples[0] === 0 && i % multiples[1] === 0) {
       console.log (words[0] + words[1]);
     } else if (i % multiples[0] === 0) {
